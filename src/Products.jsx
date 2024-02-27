1f51ef6389b312df9ebece357646076271076780
97059 <msaqel.2000@gmail.com>
2024-02-27 17:23:12 +0300

    start project

---
+++ src/Products.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Products() {
+  return (
+    <div className="products">
+      <div className="product">
+        <h2>Product one</h2>
+      </div>
+      <div className="product">
+        <h2>Product two</h2>
+      </div>
+      <div className="product">
+        <h2>Product three</h2>
+      </div>
+      <div className="product">
+        <h2>Product four</h2>
+      </div>
+      <div className="product">
+        <h2>Product five</h2>
+      </div>
+    </div>
+  );
+}
